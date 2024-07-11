fb3425bb5e725a21d6fe2100c65da2d1d253de1d
Marek Rychlik <rychlik@arizona.edu>
2024-07-11 15:17:32 -0700

    *** empty log message ***

--- alternates/gradescope-matlab/autograde_prog.m
+++ alternates/gradescope-matlab/autograde_prog.m
@@ -59,7 +59,7 @@ end
 results = struct('tests',tests);
 
 json_results = jsonencode(results,'PrettyPrint',true)
-fid = fopen(json_filename,'w');
+fid = fopen(json_filename,'a+');
 disp(['FileId:"',fid,'"']);
 fwrite(fid,json_results);
 fclose(fid);
